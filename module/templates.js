6369c64caef0499d13a61aa8c916d92de1eff0f8
Dan <daniel.gomme@yahoo.co.uk>
2020-11-27 20:41:55 +0000

    Added templates/handlebars partials
    The grid of character stats (INT, EMP, etc) will use one. `templates.js`
    provides a function for foundry to cache rendered handlebars templates
    for speed. This is loaded and run by `cyberpunk.js`. Any new handlebars
    partial used will need to be added to `templates.js`.

---
+++ module/templates.js
@@ -0,0 +1,12 @@
+/**
+ * Define a set of template paths to pre-load
+ * Pre-loaded templates are compiled and cached for fast access when rendering
+ * @return {Promise}
+ */
+export const preloadHandlebarsTemplates = async function() {
+    // Path to partial from foundry path. For cyberpunk, is "systems/cyberpunk2020/templates/actor/parts/___.html"
+    return loadTemplates([
+        "systems/cyberpunk2020/templates/actor/parts/statsgrid.html"
+    ]);
+  };
+  
