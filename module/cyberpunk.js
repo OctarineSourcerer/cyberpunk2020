985fc68a2d9c563bc2ab1e74a76b167e444ca3e1
Dan <daniel.gomme@yahoo.co.uk>
2020-11-28 21:12:20 +0000

    Cleanse some Boilerplate code
    Some code copied from the Boilerplate system removed. Some of it is
    likely to be useful later, but for now it's just cruft.

--- module/cyberpunk.js
+++ module/cyberpunk.js
@@ -1,4 +1,3 @@
-// Import Modules
 import { CyberpunkActor } from "./actor/actor.js";
 import { CyberpunkActorSheet } from "./actor/actor-sheet.js";
 import { CyberpunkItem } from "./item/item.js";
@@ -8,111 +7,36 @@ import { preloadHandlebarsTemplates } from "./templates.js";
 
 Hooks.once('init', async function () {
 
-    // Place classes in their own namespace for later reference.
+    // Place classes in system namespace for later reference.
     game.cyberpunk = {
         CyberpunkActor,
         CyberpunkItem,
-        rollItemMacro
     };
-
-    // /** Don't think this is necessary after setting in system.json
-    //  * Set an initiative formula for the system
-    //  * @type {String}
-    //  */
-    // CONFIG.Combat.initiative = {
-    //     formula: "1d20 + @abilities.dex.mod",
-    //     decimals: 2
-    // };
 
     // Define custom Entity classes
     CONFIG.Actor.entityClass = CyberpunkActor;
     CONFIG.Item.entityClass = CyberpunkActor;
 
-    // Register sheet application classes
+    // Register sheets, unregister original core sheets
     Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet("cyberpunk", CyberpunkActorSheet, { makeDefault: true });
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("cyberpunk", CyberpunkItemSheet, { makeDefault: true });
 
-    // If you need to add Handlebars helpers, here are a few useful examples:
-    Handlebars.registerHelper('concat', function () {
-        var outStr = '';
-        for (var arg in arguments) {
-            if (typeof arguments[arg] != 'object') {
-                outStr += arguments[arg];
-            }
-        }
-        return outStr;
-    });
-
     Handlebars.registerHelper('toLowerCase', function (str) {
         return str.toLowerCase();
     });
 
-    Handlebars.registerHelper("loop", function(amount, options) {
-        var ret = "";
-        
+    // Repeat what's inside it X times.
+    // Useful for testing the damage track. Use as, for example, {{#loop 4}}whatyouwanttorepeat{{/loop}}
+    Handlebars.registerHelper("repeat", function(amount, options) {
+        var result = "";
         for (var i = 0; i < amount; i++) {
-            ret = ret + options.fn(this);
+            result = result + options.fn(this);
         }
-        
         return ret;
     });
 
+    // Register and preload templates with Foundry. See templates.js for usage
     preloadHandlebarsTemplates();
 });
-
-Hooks.once("ready", async function () {
-    // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-    Hooks.on("hotbarDrop", (bar, data, slot) => createBoilerplateMacro(data, slot));
-});
-
-/* -------------------------------------------- */
-/*  Hotbar Macros                               */
-/* -------------------------------------------- */
-
-/**
- * Create a Macro from an Item drop.
- * Get an existing item macro if one exists, otherwise create a new one.
- * @param {Object} data     The dropped data
- * @param {number} slot     The hotbar slot to use
- * @returns {Promise}
- */
-async function createBoilerplateMacro(data, slot) {
-    if (data.type !== "Item") return;
-    if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
-    const item = data.data;
-
-    // Create the macro command
-    const command = `game.boilerplate.rollItemMacro("${item.name}");`;
-    let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
-    if (!macro) {
-        macro = await Macro.create({
-            name: item.name,
-            type: "script",
-            img: item.img,
-            command: command,
-            flags: { "boilerplate.itemMacro": true }
-        });
-    }
-    game.user.assignHotbarMacro(macro, slot);
-    return false;
-}
-
-/**
- * Create a Macro from an Item drop.
- * Get an existing item macro if one exists, otherwise create a new one.
- * @param {string} itemName
- * @return {Promise}
- */
-function rollItemMacro(itemName) {
-    const speaker = ChatMessage.getSpeaker();
-    let actor;
-    if (speaker.token) actor = game.actors.tokens[speaker.token];
-    if (!actor) actor = game.actors.get(speaker.actor);
-    const item = actor ? actor.items.find(i => i.name === itemName) : null;
-    if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
-
-    // Trigger the item roll
-    return item.roll();
-}
