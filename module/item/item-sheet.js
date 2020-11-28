56bd5349d01c2f6da7bbd71068c159b1b6b8a203
Dan <daniel.gomme@yahoo.co.uk>
2020-11-28 21:40:27 +0000

    Cleanse rest of Boilerplate extras
    Remove boilerplate code from the other places. Again, some of it is
    likely to end up useful when we add some interactivity, but the code's
    still in boilerplate and previous commits.

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -53,6 +53,6 @@ export class CyberpunkItemSheet extends ItemSheet {
     // Everything below here is only needed if the sheet is editable
     if (!this.options.editable) return;
 
-    // Roll handlers, click handlers, etc. would go here.
+    // Roll handlers, click handlers, etc. would go here, same as actor sheet.
   }
 }
--- module/item/item.js
+++ module/item/item.js
@@ -11,17 +11,6 @@ export class CyberpunkItem extends Item {
    * @private
    */
   async roll() {
-    // Basic template rendering data
-    const token = this.actor.token;
-    const item = this.data;
-    const actorData = this.actor ? this.actor.data.data : {};
-    const itemData = item.data;
-
-    let roll = new Roll('d20+@abilities.str.mod', actorData);
-    let label = `Rolling ${item.name}`;
-    roll.roll().toMessage({
-      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-      flavor: label
-    });
+    // This is where the item would make a roll in the chat or something like that.
   }
 }
