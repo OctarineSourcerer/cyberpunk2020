a1e8401f867e593fd592a5bf9482e001295d5a72
Dan <daniel.gomme@yahoo.co.uk>
2021-02-03 15:03:23 +0000

    Actually add all the files for the weapon sheet...

---
+++ module/lookups.js
@@ -0,0 +1,10 @@
+// This is where all the magic values go, because cyberpunk has SO many of those
+export let weaponTypes = {
+    pistol: "P",
+    submachinegun: "SMG",
+    shotgun: "SHG",
+    rifle: "RIF",
+    heavy: "HVY",
+    melee: "MELEE",
+    exotic: "EX"
+}
