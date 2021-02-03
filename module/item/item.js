4f1591dacc23bc80d249a729d3703f8bec4fcc21
Dan <daniel.gomme@yahoo.co.uk>
2021-02-03 15:02:19 +0000

    Initial weapon sheet

--- module/item/item.js
+++ module/item/item.js
@@ -1,3 +1,4 @@
+import {weaponTypes} from "../lookups.js"
 /**
  * Extend the basic Item with some very simple modifications.
  * @extends {Item}
@@ -12,5 +13,22 @@ export class CyberpunkItem extends Item {
    */
   async roll() {
     // This is where the item would make a roll in the chat or something like that.
+    switch (this.type) {
+      case "weapon":
+        __weaponRoll();
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  __weaponRoll() {
+    let owner = this.actor;
+    if(owner === null) {
+      throw new Error("This item isn't owned by anyone.");
+    }
+    let isRanged = this.type === weaponTypes.melee;
+    
   }
 }
