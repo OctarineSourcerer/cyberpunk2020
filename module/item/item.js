187bb6c3044d4c448915316bc6a2de6b2e3b7bb5
Dan <daniel.gomme@yahoo.co.uk>
2021-02-04 18:35:43 +0000

    Preliminary weapon types stuff
    So... different weapons attack differently. So have enshrined the
    different kinds of weapon attacks in `lookups.js`

--- module/item/item.js
+++ module/item/item.js
@@ -25,6 +25,25 @@ export class CyberpunkItem extends Item {
     }
   }
 
+      // Now, this is gonna have to ask the player for different things depending on the weapon
+    // Apply modifiers first? p99 in book
+    // Crit fail jam roll
+    
+    // p106
+    // Automatic weapon? choose between 3-round burst, full-auto and suppressive fire
+    // 3-round = 1 target
+    // full-auto = as many targets as you wish cos screw you
+    // Suppressive fire? choose an area. save is rof/width area, minimum 2m
+
+    // Laser? How much of the charge are you using?
+    // Microwaver? regular attack, though includes path, but also roll on microwaver table
+
+    // Area effect. Miss? Roll direction, roll meters away
+    // Shotgun? Width depends on distance from character
+    // Grenades have fixed width. Throw up to 10xBOD
+    // Gas? Wind effect. Dear lord.
+
+    // Let's just pretend the unusual ranged does exist for now
   __weaponRoll() {
     let owner = this.actor;
     if(owner === null) {
--- module/lookups.js
+++ module/lookups.js
@@ -8,3 +8,37 @@ export let weaponTypes = {
     melee: "MELEE",
     exotic: "EX"
 }
+
+// How a weapon attacks. Something like pistol or an SMG have rigid rules on how they can attack, but shotguns can be regular or auto shotgun, exotic can be laser, etc. So this is for weird and special stuff that isn't necessarily covered by the weapon's type or other information
+export let rangedAttackType = {
+    auto: "AUTO",
+    // Strange ranged weapons
+    paint: "PAINT",
+    drugs: "DRUGS",
+    acid: "ACID",
+    taser: "TASER",
+    dart: "DART",
+    squirt: "SQUIRT",
+    throwable: "THROW",
+    archer: "ARCHER",
+    // Beam weapons
+    laser: "LASER",
+    microwave: "MICROWAVE",
+    // Area effect weapons
+    shotgun: "SHOTGUN",
+    autoshotgun: "AUTOSHOTGUN",
+    grenade: "GRENADE", // Separate entry from throwable because grenades have different throw distance
+    gas: "GAS",
+    flamethrow: "FLAMETHROW",
+    landmine: "LANDMINE",
+    claymore: "CLAYMORE",
+    rpg: "RPG", // Fired same as with other grenade launchers or shoulder mounts, so not sure if should be here,
+    missile: "MISSILE",
+    explosiveCharge: "EXPLOCHARGE"
+}
+
+// This lot's a bit weird, because this is for storing an *item's* attack type, so it doesn't include martial
+export let meleeAttackType = {
+    mono: "MONO", // Monokatanas, etc
+    cyberbeast: "BEAST"
+}
