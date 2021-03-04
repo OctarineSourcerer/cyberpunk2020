4e4f1ffcc259cf40fdfaec83307caa45c7674698
Dan <daniel.gomme@yahoo.co.uk>
2021-03-04 18:19:24 +0000

    Add utils.js, where properCase has moved to
    The previous commit uses this

---
+++ module/utils.js
@@ -0,0 +1,10 @@
+// Utility methods that don't really belong anywhere else
+
+export function properCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+};
