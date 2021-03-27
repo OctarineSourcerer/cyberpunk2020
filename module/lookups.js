27a8f1ef86434b23bc67ec3c579ad5defd80f1c1
Dan <daniel.gomme@yahoo.co.uk>
2021-03-27 11:50:24 +0000

    Correct case of weapon lookup stuff
    These will be used in translation, so I edited the names to follow case
    conventions used thus far in the translation keys.
    
    Displaying isn't an issue, as CSS can essentially modify the cases
    displayed.

--- module/lookups.js
+++ module/lookups.js
@@ -1,41 +1,41 @@
 // This is where all the magic values go, because cyberpunk has SO many of those
 export let weaponTypes = {
-    pistol: "P",
+    pistol: "Pistol",
     submachinegun: "SMG",
-    shotgun: "SHG",
-    rifle: "RIF",
-    heavy: "HVY",
-    melee: "MELEE",
-    exotic: "EX"
+    shotgun: "Shotgun",
+    rifle: "Rifle",
+    heavy: "Heavy",
+    melee: "Melee",
+    exotic: "Exotic"
 }
 
 // How a weapon attacks. Something like pistol or an SMG have rigid rules on how they can attack, but shotguns can be regular or auto shotgun, exotic can be laser, etc. So this is for weird and special stuff that isn't necessarily covered by the weapon's type or other information
 // If we change attack type to be an array, we could say, have ["BEAM" "LASER"]
 export let rangedAttackType = {
-    auto: "AUTO",
+    auto: "Auto",
     // Strange ranged weapons
-    paint: "PAINT",
-    drugs: "DRUGS",
-    acid: "ACID",
-    taser: "TASER",
-    dart: "DART",
-    squirt: "SQUIRT",
-    throwable: "THROW",
-    archer: "ARCHER",
+    paint: "Paint",
+    drugs: "Drugs",
+    acid: "Acid",
+    taser: "Taser",
+    dart: "Dart",
+    squirt: "Squirt",
+    throwable: "Throw",
+    archer: "Archer",
     // Beam weapons
-    laser: "LASER",
-    microwave: "MICROWAVE",
+    laser: "Laser",
+    microwave: "Microwave",
     // Area effect weapons
-    shotgun: "SHOTGUN",
-    autoshotgun: "AUTOSHOTGUN",
-    grenade: "GRENADE", // Separate entry from throwable because grenades have different throw distance
-    gas: "GAS",
-    flamethrow: "FLAMETHROW",
-    landmine: "LANDMINE",
-    claymore: "CLAYMORE",
+    shotgun: "Shotgun",
+    autoshotgun: "Autoshotgun",
+    grenade: "Grenade", // Separate entry from throwable because grenades have different throw distance
+    gas: "Gas",
+    flamethrow: "Flamethrow",
+    landmine: "Landmine",
+    claymore: "Claymore",
     rpg: "RPG", // Fired same as with other grenade launchers or shoulder mounts, so not sure if should be here,
-    missile: "MISSILE",
-    explosiveCharge: "EXPLOCHARGE"
+    missile: "Missile",
+    explosiveCharge: "Explocharge"
 }
 
 // This lot's a bit weird, because this is for storing an *item's* attack type, so it doesn't include martial
