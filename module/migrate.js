7bfc504427819abc39ff92bbcad9453fc3f95870
Dan <daniel.gomme@yahoo.co.uk>
2021-05-27 13:27:45 +0100

    Give source fields to pistols and rifles
    TODO: Put the sources into the source field instead of flavor

--- module/migrate.js
+++ module/migrate.js
@@ -18,16 +18,8 @@ export async function migrateWorld() {
     for(let item of game.items.entities) {
         migrateEntity(item);
     }
-    for(let compendium of game.packs.entries.filter(pack => !pack.locked)) {
-        console.log(`Updating entities in compendium ${compendium.metadata.label}`);
-        let entityIds = compendium.index.map(e => e._id);
-        entityIds.forEach(async (id) => {
-            let entity = await compendium.getEntity(id);
-            migrateEntity(entity, async (entity, updateData) => {
-                updateData._id = id;
-                await compendium.updateEntity(updateData);
-            });
-        });
+    for(let compendium of game.packs.entries) {
+        migrateCompendium(compendium);
     }
     game.settings.set("cyberpunk", "systemMigrationVersion", game.system.data.version);
     ui.notifications.info(`Cyberpunk2020 System Migration to version ${game.system.data.version} completed!`, {permanent: true});
@@ -115,8 +107,10 @@ export function migrateItemData(itemData) {
     // No need to migrate items currently
     let updateData = {}
     let data = itemData.data;
+    let itemTemplates = game.system.template.Item[itemData.type].templates;
 
-    if(data.source === undefined) {
+    if(itemTemplates.includes("common") && data.source === undefined) {
+        console.log(`${itemData.name} has no source field. Giving it one.`)
         updateData["data.source"] = "";
     }
     if(itemData.type == "weapon") {
@@ -126,4 +120,20 @@ export function migrateItemData(itemData) {
         }
     }
     return updateData;
+}
+
+export function migrateCompendium(compendium) {
+    if(compendium.locked) {
+        console.log(`Not migrating compendium ${compendium.metadata.label}, as it is locked`);
+        return
+    }
+    console.log(`Updating entities in compendium ${compendium.metadata.label}`);
+    let entityIds = compendium.index.map(e => e._id);
+    entityIds.forEach(async (id) => {
+        let entity = await compendium.getEntity(id);
+        migrateEntity(entity, async (entity, updateData) => {
+            updateData._id = id;
+            await compendium.updateEntity(updateData);
+        });
+    });
 }
