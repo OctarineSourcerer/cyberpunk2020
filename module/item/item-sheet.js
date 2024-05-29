eec8c8952907e42ec9d941d49907e389c0ba9385
Dan <daniel.gomme@yahoo.co.uk>
2024-05-29 09:48:42 +0100

    Change to foundry.utils.mergeObject
    The global one was deprecated

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -10,7 +10,7 @@ export class CyberpunkItemSheet extends ItemSheet {
 
   /** @override */
   static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
+    return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["cyberpunk", "sheet", "item"],
       width: 520,
       height: 480,
