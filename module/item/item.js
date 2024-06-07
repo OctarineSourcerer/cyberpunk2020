eab8a7063e90288f081fa61ebcab0f4ab028dc3c
Dan <daniel.gomme@yahoo.co.uk>
2024-06-07 12:30:49 +0100

    Take into account number of targets when full auto
    When full-auto, fire multiple times.
    
    Firing the weapon should also now actually change the number of shots
    left.

--- module/item/item.js
+++ module/item/item.js
@@ -275,38 +275,45 @@ export class CyberpunkItem extends Item {
     // The kind of distance we're attacking at, so we can display Close: <50m or something like that
     let actualRangeBracket = rangeResolve[attackMods.range](system.range);
     let DC = rangeDCs[attackMods.range];
-    let attackRoll = await this.attackRoll(attackMods);
-
-    let roundsFired = Math.min(system.shotsLeft, system.rof);
-    let roundsHit = Math.min(roundsFired, attackRoll.total - DC);
-    if(roundsHit < 0) {
-      roundsHit = 0;
-    }
-    let areaDamages = {};
-    // Roll damage for each of the bullets that hit
-    for(let i = 0; i < roundsHit; i++) {
-      let damageRoll = await new Roll(system.damage).evaluate();
-      let location = (await rollLocation(attackMods.targetActor, attackMods.targetArea)).areaHit;
-      if(!areaDamages[location]) {
-        areaDamages[location] = [];
+    let targetCount = attackMods.targets.length || 1;
+    
+    // This is a somewhat flawed multi-target thing - given target tokens, we could calculate distance (& therefore penalty) for each, and apply damage to them
+    let rolls = [];
+    for (let i = 0; i < targetCount; i++) {
+      let attackRoll = await this.attackRoll(attackMods);
+      let roundsFired = Math.min(system.shotsLeft, system.rof / targetCount);
+      await this.update({shotsLeft: system.shotsLeft - roundsFired})
+      let roundsHit = Math.min(roundsFired, attackRoll.total - DC);
+      if(roundsHit < 0) {
+        roundsHit = 0;
       }
-      areaDamages[location].push(damageRoll);
-    }
-    let templateData = {
-      range: attackMods.range,
-      toHit: DC,
-      attackRoll: attackRoll,
-      fired: roundsFired,
-      hits: roundsHit,
-      hit: roundsHit > 0,
-      areaDamages: areaDamages,
-      locals: {
-        range: { range: actualRangeBracket }
+      let areaDamages = {};
+      // Roll damage for each of the bullets that hit
+      for(let i = 0; i < roundsHit; i++) {
+        let damageRoll = await new Roll(system.damage).evaluate();
+        let location = (await rollLocation(attackMods.targetActor, attackMods.targetArea)).areaHit;
+        if(!areaDamages[location]) {
+          areaDamages[location] = [];
+        }
+        areaDamages[location].push(damageRoll);
       }
+      let templateData = {
+        range: attackMods.range,
+        toHit: DC,
+        attackRoll: attackRoll,
+        fired: roundsFired,
+        hits: roundsHit,
+        hit: roundsHit > 0,
+        areaDamages: areaDamages,
+        locals: {
+          range: { range: actualRangeBracket }
+        }
+      }
+      let roll = new Multiroll(localize("Autofire"), `${localize("Range")}: ${localizeParam(attackMods.range, {range: actualRangeBracket})}`);
+      roll.execute(undefined, "systems/cyberpunk2020/templates/chat/multi-hit.hbs", templateData);
+      rolls.push(roll);
     }
-    let roll = new Multiroll(localize("Autofire"), `${localize("Range")}: ${localizeParam(attackMods.range, {range: actualRangeBracket})}`);
-    roll.execute(undefined, "systems/cyberpunk2020/templates/chat/multi-hit.hbs", templateData);
-    return roll;
+    return rolls;
   }
 
   async __threeRoundBurst(attackMods) {
@@ -346,6 +353,8 @@ export class CyberpunkItem extends Item {
     }
     let roll = new Multiroll(localize("ThreeRoundBurst"));
     roll.execute(undefined, "systems/cyberpunk2020/templates/chat/multi-hit.hbs", templateData);
+    this.update({shotsLeft: system.shotsLeft - roundsFired})
+    return roll;
   }
 
   async __semiAuto(attackMods) {
@@ -361,6 +370,7 @@ export class CyberpunkItem extends Item {
       .addRoll(damageRoll, {name: localize("Damage")})
       .addRoll(locationRoll.roll, {name: localize("Location"), flavor: locationRoll.areaHit });
     bigRoll.defaultExecute({img:this.img});
+    this.update({shotsLeft: system.shotsLeft - 1})
     return bigRoll;
   }
   async __meleeBonk(attackMods) {
